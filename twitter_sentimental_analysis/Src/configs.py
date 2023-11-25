3140e37c62ad09512e6de68b0d2506bc50da6fbb
Madhan Raj Moorthy <51472231+moorthymadhanraj@users.noreply.github.com>
2023-11-25 21:15:48 +0100

    Add files via upload

---
+++ twitter_sentimental_analysis/Src/configs.py
@@ -0,0 +1,14 @@
+configs = {
+            "port":3334,
+            "twitter_credentials": {
+                                     "consumer_key":"oQ0HyZdVuns6uMNPyEO3zoVE7",
+                                     "consumer_secret":"mIvpUjzAqdwpxKQm0yMvnmA1PycLvr4u4KFCPvN8pAV8GU6BUm",
+                                     "access_token":"1442544730986860552-qffg1VVbkuCrWE8wuTxB4kkZ5Isms9",
+                                     "access_token_secret":"CmQacrgeXyPjEZqCAJHAakdYvgzyrMCuIY6StCocKTSaz"
+                                   },
+            "amazon_credentials": {
+                                     "AWS_SERVER_PUBLIC_KEY":"AKIA2ZVY4AKDM7VZC5J6",
+                                     "AWS_SERVER_SECRET_KEY":"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"
+                                   },
+
+          }
